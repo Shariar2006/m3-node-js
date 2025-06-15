96c715dc5321e7e2406c3953ce88b34621fd25dd
Shariar2006 <shariarnafis86@gmail.com>
2025-06-15 19:30:38 +0600

    send dynamic responsive

--- module-2/todoapp/server.js
+++ module-2/todoapp/server.js
@@ -1,7 +1,15 @@
 const http = require('http');
 
 const server = http.createServer((req, res) => {
-    res.end('welcome to server')
+    console.log(req.url, req.method)
+    if(req.url === '/todos' & req.method === 'GET'){
+        res.end('get all todos data')
+    }
+    else if(req.url === '/todos/create-todo' & req.method === 'POST'){
+        res.end('create a todo')
+    }else{
+        res.end('route not found')
+    }
 })
 
 server.listen(3080, '127.0.0.1', () => {

dd2d45feacd8666d00c20a4e4a6a2f0ee546dbc7
Shariar2006 <shariarnafis86@gmail.com>
2025-06-15 19:41:19 +0600

    send json responsive get path

--- module-2/todoapp/server.js
+++ module-2/todoapp/server.js
@@ -3,6 +3,9 @@ const http = require('http');
 const server = http.createServer((req, res) => {
     console.log(req.url, req.method)
     if(req.url === '/todos' & req.method === 'GET'){
+        res.writeHead(201, {
+            "content-type" : "application/json"
+        })
         res.end('get all todos data')
     }
     else if(req.url === '/todos/create-todo' & req.method === 'POST'){

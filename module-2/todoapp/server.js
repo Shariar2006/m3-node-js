87d73c66179d6993bc8947ca246aab90ecb9ecf6
Shariar2006 <shariarnafis86@gmail.com>
2025-06-17 21:11:05 +0600

    get json data using file system

--- module-2/todoapp/server.js
+++ module-2/todoapp/server.js
@@ -1,12 +1,17 @@
 const http = require('http');
+const path = require('path');
+const fs = require('fs');
+
+const filePath = path.join(__dirname, "./db/todo.json")
 
 const server = http.createServer((req, res) => {
     console.log(req.url, req.method)
     if(req.url === '/todos' & req.method === 'GET'){
+        const data = fs.readFileSync(filePath, {encoding: "utf-8"})
         res.writeHead(201, {
             "content-type" : "application/json"
         })
-        res.end('get all todos data')
+        res.end(data)
     }
     else if(req.url === '/todos/create-todo' & req.method === 'POST'){
         res.end('create a todo')

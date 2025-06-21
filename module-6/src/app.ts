f2cdbf1a87d09540dee2895cdaddcaa799e8dabe
Shariar2006 <shariarnafis86@gmail.com>
2025-06-21 23:33:32 +0600

    create a note using mongoose

--- module-6/src/app.ts
+++ module-6/src/app.ts
@@ -1,6 +1,27 @@
 import express, { Application, Request, Response } from 'express'
+import { Schema, model } from 'mongoose'
 
 const app: Application = express()
+
+const noteSchema = new Schema({
+    title: String,
+    content: String
+})
+
+const Note = model("Note", noteSchema)
+
+app.post('/create-note', (req: Request, res: Response)=>{
+    const myNote = new Note({
+        title: 'learning mongoose',
+        content: "I am learning mongoose"
+    })
+    myNote.save()
+    
+    res.status(200).json({
+        message: 'successfully created a note',
+        note: myNote
+    })
+})
 
 app.get('/', (req: Request, res: Response)=>{
     res.send('server starting')
--- module-6/src/server.ts
+++ module-6/src/server.ts
@@ -1,11 +1,12 @@
 import mongoose from "mongoose";
 import app from "./app";
+import { mongooseURI } from "./mongooseURI";
 
 let server;
 const PORT = 3080
 async function main() {
     try {
-        await mongoose.connect('')
+        await mongoose.connect(mongooseURI)
        console.log('connect mongoose')
         server = app.listen(PORT, ()=>{
             console.log(`app successfully listen ${PORT}`)

0e278151455fdd894aede7ab0183c8afe1baef37
Shariar2006 <shariarnafis86@gmail.com>
2025-06-22 17:55:20 +0600

    create note controller and model

---
+++ module-6/src/app/controllers/note.controller.ts
@@ -0,0 +1,60 @@
+import express, { Request, Response } from 'express'
+import { Note } from '../models/note.model';
+
+export const noteRoutes = express.Router()
+
+// create a note
+noteRoutes.post('/create-note', async (req: Request, res: Response) => {
+    const body = req.body;
+
+    const note = await Note.create(body)
+
+    res.status(200).json({
+        message: 'successfully created a note',
+        note
+    })
+})
+
+// get note
+noteRoutes.get('/', async (req: Request, res: Response) => {
+    const notes = await Note.find()
+
+    res.status(200).json({
+        message: 'successfully get all note',
+        notes
+    })
+})
+
+// get a note
+noteRoutes.get('/:id', async (req: Request, res: Response) => {
+    const noteID = req.params.id
+    const note = await Note.findById(noteID)
+
+    res.status(200).json({
+        message: 'successfully get a note',
+        note
+    })
+})
+
+// update a note
+noteRoutes.patch('/:id', async (req: Request, res: Response) => {
+    const noteID = req.params.id
+    const noteBody = req.body
+    const note = await Note.findByIdAndUpdate(noteID, noteBody, {new: true})
+
+    res.status(200).json({
+        message: 'successfully updated a note',
+        note
+    })
+})
+
+// delete a note
+noteRoutes.delete('/delete-note/:id', async (req: Request, res: Response) => {
+    const noteID = req.params.id
+    const note = await Note.findByIdAndDelete(noteID)
+
+    res.status(200).json({
+        message: 'successfully delete a note',
+        note
+    })
+})
---
+++ module-6/src/app/models/note.model.ts
@@ -0,0 +1,34 @@
+import { Schema, model } from "mongoose"
+
+const noteSchema = new Schema({
+    title: {
+        type: String,
+        required: true,
+        trim: true
+    },
+    content: {
+        type: String,
+        default: ''
+    },
+    category: {
+        type: String,
+        enum: ['personal', 'office', 'other'],
+        default: 'personal'
+    },
+    tags: {
+        label: {
+            type: String,
+            required: true
+        },
+        color: {
+            type: String,
+            default: 'gray'
+        }
+    }
+},
+{
+    versionKey: false,
+    timestamps: true
+})
+
+export const Note = model("Note", noteSchema)

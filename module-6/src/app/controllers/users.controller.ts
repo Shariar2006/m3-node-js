2498068f62cad3b316fddff39883cd78eaa8a0f9
Shariar2006 <shariarnafis86@gmail.com>
2025-06-22 20:21:48 +0600

    add zod validation

--- module-6/src/app/controllers/users.controller.ts
+++ module-6/src/app/controllers/users.controller.ts
@@ -1,18 +1,35 @@
 import express, { Request, Response } from 'express'
 import { User } from '../models/users.model';
+import { z } from 'zod';
 
 export const userRoutes = express.Router()
 
+const createUserZodSchema = z.object({
+    firstName: z.string(),
+    lastName: z.string(),
+    role: z.string(),
+    email: z.string(),
+    password: z.string(),
+})
+
 // create a user
 userRoutes.post('/create-user', async (req: Request, res: Response) => {
-    const body = req.body;
+    try {
+        const body = await createUserZodSchema.parseAsync(req.body);
 
-    const user = await User.create(body)
+        const user = await User.create(body)
 
-    res.status(200).json({
-        message: 'successfully created a user',
-        user
-    })
+        res.status(200).json({
+            message: 'successfully created a user',
+            user
+        })
+    } catch (error: any) {
+        res.status(400).json({
+            success: false,
+            message: error.message,
+            error
+        })
+    }
 })
 
 // get user
@@ -40,7 +57,7 @@ userRoutes.get('/:id', async (req: Request, res: Response) => {
 userRoutes.patch('/:id', async (req: Request, res: Response) => {
     const userID = req.params.id
     const userBody = req.body
-    const user = await User.findByIdAndUpdate(userID, userBody, {new: true})
+    const user = await User.findByIdAndUpdate(userID, userBody, { new: true })
 
     res.status(200).json({
         message: 'successfully updated a user',
